cc031afd45911e3cf320dafac6f957e280745899
Lisa Fujita <lisafujita90@gmail.com>
2024-10-24 11:14:03 -0400

    props and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
 // Write your Color component here
+import { useState } from 'react';
+
+const Color = ({ color, setSelectedColor, selectedColor }) => {
+  const isSelected = color === selectedColor ? 'selected' : '';
+  return (
+    <div
+      className={`${color} ${isSelected}`}
+      onClick={() => setSelectedColor(color)}
+      // style={{ backgroundColor: color, width: '50px', height: '50px' }}
+    ></div>
+  );
+};
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState('');
+
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color
+          color="orange"
+          setSelectedColor={setSelectedColor}
+          selectedColor={selectedColor}
+        />
+        <Color
+          color="yellow"
+          setSelectedColor={setSelectedColor}
+          selectedColor={selectedColor}
+        />
+        <Color
+          color="green"
+          setSelectedColor={setSelectedColor}
+          selectedColor={selectedColor}
+        />
+      </div>
     </div>
   );
 };
